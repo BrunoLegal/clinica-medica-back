e7bbc43ac43f428187dad8976587bd8a85d1a2f0
alfredaum <alfredojose1408@hotmail.com>
2024-06-05 21:50:48 -0300

    ConvenioController

--- clinica-administrativo/src/main/java/br/edu/imepac/controllers/ConvenioController.java
+++ clinica-administrativo/src/main/java/br/edu/imepac/controllers/ConvenioController.java
@@ -1,8 +1,13 @@
 package br.edu.imepac.controllers;
+import br.edu.imepac.dtos.ConvenioCreateRequest;
+import br.edu.imepac.dtos.ConvenioDto;git 
 import br.edu.imepac.services.ConvenioService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -10,6 +15,38 @@ import java.util.List;
 @RequestMapping("convenio")
 public class ConvenioController {
 
+    private static final Logger logger = LoggerFactory.getLogger(ConvenioController.class);
     @Autowired
-    private ConvenioService convenioservice;
+    private ConvenioService convenioService;
+
+    @PostMapping
+    public ResponseEntity<ConvenioDto> saveConvenio(@RequestBody ConvenioCreateRequest convenioCreateRequest){
+        logger.info("Request ConvenioCreateRequest");
+        ConvenioDto saveConvenio = convenioService.save(convenioCreateRequest);
+        return new ResponseEntity<>(saveConvenio, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ConvenioDto>> listAllConvenios(){
+        List<ConvenioDto> convenios = convenioService.findAll();
+        return new ResponseEntity<>(convenios, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<ConvenioDto> updateConvenio(@PathVariable Long id, @RequestBody ConvenioDto convenioDetails){
+        ConvenioDto updateConvenio = convenioService.update(id, convenioDetails);
+        if(updateConvenio != null){
+            return new ResponseEntity<>(updateConvenio, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteConvenio(@PathVariable Long id) {
+        convenioService.delete(id);
+    }
 }
+
