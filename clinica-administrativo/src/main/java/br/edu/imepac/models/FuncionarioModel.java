a8576a8bb64ae35744b139d4b79de1bcd86c3d4e
Sucas63 <lucas.moreira@aluno.imepac.edu.br>
2024-06-05 22:21:19 -0300

    alterações funcionario acrescentando mais coisas

--- clinica-administrativo/src/main/java/br/edu/imepac/models/FuncionarioModel.java
+++ clinica-administrativo/src/main/java/br/edu/imepac/models/FuncionarioModel.java
@@ -18,11 +18,33 @@ public class FuncionarioModel {
     private Long id;
 
     @NotNull
-    private String nome;
-    @ManyToOne
-    @JoinColumn(name = "id_especialidade")
-    private EspecialidadeModel especialidade;
+    private String nome_Funcionario;
     @NotNull
-    private String CRM;
-
+    private String numero_Rg;
+    @NotNull
+    private String orgao_Emissor;
+    @NotNull
+    private String numero_Cpf;
+    @NotNull
+    private String endereco;
+    @NotNull
+    private String numero;
+    @NotNull
+    private String complemento;
+    @NotNull
+    private String bairro;
+    @NotNull
+    private String cidade;
+    @NotNull
+    private String estado;
+    @NotNull
+    private String telefone;
+    @NotNull
+    private String celular;
+    @NotNull
+    private String numero_Ctps;
+    @NotNull
+    private String numero_Pis;
+    @NotNull
+    private String Data;
 }
