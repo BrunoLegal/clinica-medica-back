d0ae8b6737a011c8326e4c261feede498316b8d6
Sucas63 <lucas.moreira@aluno.imepac.edu.br>
2024-06-10 20:47:25 -0300

    Revertendo erro Funcionario

--- clinica-administrativo/src/main/java/br/edu/imepac/controllers/FuncionarioController.java
+++ clinica-administrativo/src/main/java/br/edu/imepac/controllers/FuncionarioController.java
@@ -13,51 +13,6 @@ import java.util.List;
 @RestController
 @RequestMapping("funcionario")
 public class FuncionarioController {
-    @Autowired
-    private FuncionarioService funcionarioService;
-    @GetMapping
-    public ResponseEntity<List<FuncionarioDto>> listFuncionarios(){
-        List<FuncionarioDto> list = funcionarioService.findAll();
-        if(list.isEmpty()){
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }else{
-            return new ResponseEntity<>(list, HttpStatus.OK);
-        }
-    }
 
-    @GetMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<FuncionarioDto> findFuncionario(@PathVariable Long id){
-        FuncionarioDto funcionarioDto = funcionarioService.findById(id);
-        if(funcionarioDto != null){
-            return new ResponseEntity<>(funcionarioDto, HttpStatus.OK);
-        }else{
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-
-    @PostMapping
-    public ResponseEntity<FuncionarioDto> saveFuncionario(@RequestBody FuncionarioCreateRequest funcionarioCreateRequest){
-        FuncionarioDto funcionarioDto = funcionarioService.save(funcionarioCreateRequest);
-        return new ResponseEntity<>(funcionarioDto, HttpStatus.CREATED);
-    }
-
-    @PutMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<FuncionarioDto> updateFuncionario(@RequestBody FuncionarioDto funcionarioData, @PathVariable Long id){
-        FuncionarioDto funcionarioDto = funcionarioService.update(id, funcionarioData);
-        if(funcionarioDto != null){
-            return new ResponseEntity<>(funcionarioDto, HttpStatus.OK);
-        }else{
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-    @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.OK)
-    public void deleteFuncionario(@PathVariable Long id){
-        funcionarioService.delete(id);
-    }
 
 }
