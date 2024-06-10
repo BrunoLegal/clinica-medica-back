fbb757c0d93316ab0cf6657282be45d419d5f25a
Sucas63 <lucas.moreira@aluno.imepac.edu.br>
2024-06-10 20:26:37 -0300

    Listas do funcionario

--- clinica-administrativo/src/main/java/br/edu/imepac/dtos/FuncionarioDto.java
+++ clinica-administrativo/src/main/java/br/edu/imepac/dtos/FuncionarioDto.java
@@ -1,4 +1,27 @@
 package br.edu.imepac.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FuncionarioDto {
+
+    private String nome_Funcionario;
+    private String numero_Rg;
+    private String orgao_Emissor;
+    private String numero_Cpf;
+    private String endereco;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String telefone;
+    private String celular;
+    private String numero_Ctps;
+    private String numero_Pis;
+    private String Data;
 }
