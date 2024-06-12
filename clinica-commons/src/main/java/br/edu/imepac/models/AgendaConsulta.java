5d5a07a0bc1982045bd10c1743301e58bb33e4c1
Bruno Antônio Joanucci <bruno.joanucci@aluno.imepac.edu.br>
2024-06-12 19:58:42 -0300

    AgendaConsulta Criada

---
+++ clinica-commons/src/main/java/br/edu/imepac/models/AgendaConsulta.java
@@ -0,0 +1,43 @@
+package br.edu.imepac.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "agenda_consulta")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AgendaConsulta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private UsuarioModel usuario;
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "paciente_id")
+    private PacienteModel paciente;
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "medico_id")
+    private MedicoModel medico;
+    @NotNull
+    private Date data;
+    @NotNull
+    private LocalTime hora;
+    @NotNull
+    private Boolean retorno;
+    @NotNull
+    private Boolean cancelado;
+    @Column(name = "motivo_cancelamento")
+    private String motivoCancelamento;
+}
