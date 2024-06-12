63ca38c67cbf13b4c81384ace399f0605af7b795
Bruno Antônio Joanucci <bruno.joanucci@aluno.imepac.edu.br>
2024-06-12 19:45:14 -0300

    Adicionado construtores pelo lombok no ConvenioModel

--- clinica-commons/src/main/java/br/edu/imepac/models/ConvenioModel.java
+++ clinica-commons/src/main/java/br/edu/imepac/models/ConvenioModel.java
@@ -1,11 +1,15 @@
 package br.edu.imepac.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "convenios")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ConvenioModel {
 
     @Id
