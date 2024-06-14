cd77ed884c1244235a362e561c2cac690e746209
Arthur <arthurmr2016t@gmail.com>
2024-06-14 11:00:23 -0300

    Atendimento
    - Controller, dtos, model, repository e service do atendimento
    - Nenhum erro aparente
    - Não Testei
    - Não liguei ao administrativo

--- clinica-atendimento/src/main/java/br/edu/imepac/dtos/AtendimentoCreateRequest.java
+++ clinica-atendimento/src/main/java/br/edu/imepac/dtos/AtendimentoCreateRequest.java
@@ -1,5 +1,6 @@
 package br.edu.imepac.dtos;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +11,12 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AtendimentoCreateRequest {
+    @NotNull
     private Long pacienteId;
+    @NotNull
     private Long medicoId;
+    @NotNull
     private LocalDateTime dataHora;
+    @NotNull
     private String descricao;
 }
--- clinica-atendimento/src/main/java/br/edu/imepac/services/AtendimentoService.java
+++ clinica-atendimento/src/main/java/br/edu/imepac/services/AtendimentoService.java
@@ -7,6 +7,10 @@ import br.edu.imepac.repository.AtendimentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Service
 public class AtendimentoService {
 
@@ -22,5 +26,41 @@ public class AtendimentoService {
 
         AtendimentoModel saveAtendimento = atendimentoRepository.save(atendimentoModel);
         return toDto(saveAtendimento);
+    }
+
+    public List<AtendimentoDto> getAllAtendimentos() {
+        return atendimentoRepository.findAll().stream().map(this::toDto).collect(Collectors.toList());
+    }
+
+    public Optional<AtendimentoDto> getAtendimentoById(Long id) {
+        return atendimentoRepository.findById(id).map(this::toDto);
+    }
+
+    public AtendimentoDto updateAtendimento(Long id, AtendimentoCreateRequest atendimentoCreateRequest) {
+        AtendimentoModel atendimentoModel = atendimentoRepository.findById(id).orElseThrow(() -> new RuntimeException("Atendimento não encontrado"));
+        atendimentoModel.setPacienteId(atendimentoCreateRequest.getPacienteId());
+        atendimentoModel.setMedicoId(atendimentoCreateRequest.getMedicoId());
+        atendimentoModel.setDataHora(atendimentoCreateRequest.getDataHora());
+        atendimentoModel.setDescricao(atendimentoCreateRequest.getDescricao());
+
+        AtendimentoModel updateAtendimento =  atendimentoRepository.save(atendimentoModel);
+        return toDto(updateAtendimento);
+    }
+
+    public void deleteAtendimento(Long id) {
+        atendimentoRepository.deleteById(id);
+    }
+
+
+
+    private AtendimentoDto toDto(AtendimentoModel atendimentoModel) {
+        AtendimentoDto atendimentoDto = new AtendimentoDto();
+        atendimentoDto.setId(atendimentoModel.getId());
+        atendimentoDto.setPacienteId(atendimentoModel.getPacienteId());
+        atendimentoDto.setMedicoId(atendimentoModel.getMedicoId());
+        atendimentoDto.setDataHora(atendimentoModel.getDataHora());
+        atendimentoDto.setDescricao(atendimentoModel.getDescricao());
+
+        return atendimentoDto;
     }
 }
