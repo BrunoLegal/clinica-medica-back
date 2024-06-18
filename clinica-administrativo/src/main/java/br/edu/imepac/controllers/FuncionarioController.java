a16e68674633b2fe83c1138b2b51e342468a2a4b
Sucas63 <lucas.moreira@aluno.imepac.edu.br>
2024-06-18 19:57:27 -0300

    Alterações funcionario

--- clinica-administrativo/src/main/java/br/edu/imepac/controllers/FuncionarioController.java
+++ clinica-administrativo/src/main/java/br/edu/imepac/controllers/FuncionarioController.java
@@ -17,7 +17,7 @@ public class FuncionarioController {
     @Autowired
     private FuncionarioService funcionarioService;
     @GetMapping
-    public ResponseEntity<List<FuncionarioDto>> listAllFuncionarios(){
+    public ResponseEntity<List<FuncionarioDto>> listFuncionariosd(){
         List<FuncionarioDto> list = funcionarioService.findAll();
         if(list.isEmpty()){
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
