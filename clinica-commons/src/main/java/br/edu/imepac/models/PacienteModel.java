02253e13860492758578f210308b8cea7a0ea2eb
osmarindo <osmarindo.neto@aluno.imepac.edu.br>
2024-06-26 20:17:24 -0300

    hhh

--- clinica-commons/src/main/java/br/edu/imepac/models/PacienteModel.java
+++ clinica-commons/src/main/java/br/edu/imepac/models/PacienteModel.java
@@ -16,38 +16,36 @@ public class PacienteModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull
+
     private String nome;
-    @NotNull
+
     private String numero_RG;
-    @NotNull
+
     private String orgao_Emissor;
-    @NotNull
+
     private String numero_CPF;
-    @NotNull
+
     private String endereco;
-    @NotNull
+
     private String numero;
     private String complemento;
-    @NotNull
+
     private String bairro;
-    @NotNull
+
     private String cidade;
-    @NotNull
+
     private String estado;
     private String telefone;
-    @NotNull
+
     private String celular;
-    @NotNull
+
     private String data_de_nascimento;
-    @NotNull
+
     private String sexo;
-    @NotNull
     private String tem_convenio;
     @ManyToOne
     @JoinColumn(name = "id_convenio")
     private ConvenioModel convenio;
-    @NotNull
     private String senha_acesso;
 
 }
