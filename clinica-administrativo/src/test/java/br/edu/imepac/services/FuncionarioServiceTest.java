846ed6c43b3fcd2b37bbb4978a451f657a047f65
Sucas63 <lucas.moreira@aluno.imepac.edu.br>
2024-06-26 20:59:56 -0300

    alterações

---
+++ clinica-administrativo/src/test/java/br/edu/imepac/services/FuncionarioServiceTest.java
@@ -0,0 +1,31 @@
+package br.edu.imepac.services;
+
+import br.edu.imepac.dtos.FuncionarioCreateRequest;
+import br.edu.imepac.dtos.FuncionarioDto;
+import br.edu.imepac.models.FuncionarioModel;
+import br.edu.imepac.repositories.ConvenioRepository;
+import br.edu.imepac.repositories.FuncionarioRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+
+class FuncionarioServiceTest {
+
+    @InjectMocks
+    private ConvenioService funcionarioService;
+
+    @Mock
+    private ConvenioRepository funcionarioRepository;
+}
