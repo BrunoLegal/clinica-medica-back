1e8eac43cd14f8d30631bc63609512fa51e79672
osmarindo <osmarindo.neto@aluno.imepac.edu.br>
2024-06-26 21:00:41 -0300

    hhh

--- clinica-administrativo/src/main/java/br/edu/imepac/controllers/EspecialidadeController.java
+++ clinica-administrativo/src/main/java/br/edu/imepac/controllers/EspecialidadeController.java
@@ -4,6 +4,9 @@ package br.edu.imepac.controllers;
 import br.edu.imepac.dtos.EspecialidadeCreateRequest;
 import br.edu.imepac.dtos.EspecialidadeDto;
 import br.edu.imepac.services.EspecialidadeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +20,13 @@ public class EspecialidadeController {
 
     @Autowired
     private EspecialidadeService especialidadeService;
+
+    @Operation(description = "exibe todas as especialidades salvas no banco de dados", tags = "Especialidade")
+    @ApiResponses(value = {
+            @ApiResponse( responseCode = "200", description = "retorna uma lista de todas as especialidade salvas no banco de dados"),
+            @ApiResponse( responseCode = "400", description = "erro do cliente"),
+            @ApiResponse( responseCode = "500", description = "erro do servidor")
+    })
     @GetMapping
     public ResponseEntity<List<EspecialidadeDto>> listEspecialidade(){
         List<EspecialidadeDto> list = especialidadeService.findAll();
@@ -27,6 +37,12 @@ public class EspecialidadeController {
         }
     }
 
+    @Operation(description = "exibe uma especialidade salva no banco de dados", tags = "Especialidade")
+    @ApiResponses(value = {
+            @ApiResponse( responseCode = "200", description = "retorna uma especialidade salva no banco de dados"),
+            @ApiResponse( responseCode = "400", description = "erro do cliente"),
+            @ApiResponse( responseCode = "500", description = "erro do servidor")
+    })
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<EspecialidadeDto> findEspecialidade(@PathVariable Long id){
@@ -38,6 +54,12 @@ public class EspecialidadeController {
         }
     }
 
+    @Operation(description = "Registra uma nova especialidade", tags = "Especialidade")
+    @ApiResponses(value = {
+            @ApiResponse( responseCode = "200", description = "Especialidade salva com sucesso"),
+            @ApiResponse( responseCode = "400", description = "erro do cliente"),
+            @ApiResponse( responseCode = "500", description = "erro do servidor")
+    })
 
     @PostMapping
     public ResponseEntity<EspecialidadeDto> saveEspecialidade(@RequestBody EspecialidadeCreateRequest especialidadeCreateRequest){
@@ -45,6 +67,13 @@ public class EspecialidadeController {
         return new ResponseEntity<>(especialidadeDto, HttpStatus.CREATED);
     }
 
+    @Operation(description = "atualiza os dados de uma especialidade", tags = "Especialidade")
+    @ApiResponses(value = {
+            @ApiResponse( responseCode = "200", description = "Especialidade atualizado com sucesso"),
+            @ApiResponse( responseCode = "400", description = "erro do cliente"),
+            @ApiResponse( responseCode = "404", description = "Especialidade não encontrado"),
+            @ApiResponse( responseCode = "500", description = "erro do servidor")
+    })
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<EspecialidadeDto> updateEspecialidade(@RequestBody EspecialidadeDto especialidadeData, @PathVariable Long id){
@@ -56,6 +85,12 @@ public class EspecialidadeController {
         }
     }
 
+    @Operation(description = "deleta uma especialidade", tags = "Especialidade")
+    @ApiResponses(value = {
+            @ApiResponse( responseCode = "200", description = "Especialidade deletado com sucesso"),
+            @ApiResponse( responseCode = "400", description = "erro do cliente"),
+            @ApiResponse( responseCode = "500", description = "erro do servidor")
+    })
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteEspecialidade(@PathVariable Long id){
--- clinica-agendamento/src/main/java/br/edu/imepac/controllers/PacienteController.java
+++ clinica-agendamento/src/main/java/br/edu/imepac/controllers/PacienteController.java
@@ -3,6 +3,9 @@ package br.edu.imepac.controllers;
 import br.edu.imepac.dtos.PacienteCreateRequest;
 import br.edu.imepac.dtos.PacienteDto;
 import br.edu.imepac.services.PacienteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +19,13 @@ import java.util.List;
 public class PacienteController {
     @Autowired
     private PacienteService pacienteService;
+
+    @Operation(description = "exibe todos os pacientes salvos no banco de dados", tags = "Paciente")
+    @ApiResponses(value = {
+            @ApiResponse( responseCode = "200", description = "retorna uma lista de todos os pacientes salvos no banco de dados"),
+            @ApiResponse( responseCode = "400", description = "erro do cliente"),
+            @ApiResponse( responseCode = "500", description = "erro do servidor")
+    })
     @GetMapping
     public ResponseEntity<List<PacienteDto>> listPaciente(){
         List<PacienteDto> list = pacienteService.findAll();
@@ -26,6 +36,12 @@ public class PacienteController {
         }
     }
 
+    @Operation(description = "exibe um paciente salvo no banco de dados", tags = "Paciente")
+    @ApiResponses(value = {
+            @ApiResponse( responseCode = "200", description = "retorna um paciente salvo no banco de dados"),
+            @ApiResponse( responseCode = "400", description = "erro do cliente"),
+            @ApiResponse( responseCode = "500", description = "erro do servidor")
+    })
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<PacienteDto> findPaciente(@PathVariable Long id){
@@ -37,12 +53,25 @@ public class PacienteController {
         }
     }
 
-
+    @Operation(description = "Registra um novo Paciente", tags = "Paciente")
+    @ApiResponses(value = {
+            @ApiResponse( responseCode = "200", description = "Paciente salvo com sucesso"),
+            @ApiResponse( responseCode = "400", description = "erro do cliente"),
+            @ApiResponse( responseCode = "500", description = "erro do servidor")
+    })
     @PostMapping
     public ResponseEntity<PacienteDto> savePaciente(@RequestBody PacienteCreateRequest medicoCreateRequest){
         PacienteDto pacienteDto = pacienteService.save(medicoCreateRequest);
         return new ResponseEntity<>(pacienteDto, HttpStatus.CREATED);
     }
+
+    @Operation(description = "atualiza os dados de um paciente", tags = "Paciente")
+    @ApiResponses(value = {
+            @ApiResponse( responseCode = "200", description = "Paciente atualizado com sucesso"),
+            @ApiResponse( responseCode = "400", description = "erro do cliente"),
+            @ApiResponse( responseCode = "404", description = "Paciente não encontrado"),
+            @ApiResponse( responseCode = "500", description = "erro do servidor")
+    })
 
     @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
@@ -54,6 +83,13 @@ public class PacienteController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+
+    @Operation(description = "deleta um paciente", tags = "Paciente")
+    @ApiResponses(value = {
+            @ApiResponse( responseCode = "200", description = "paciente deletado com sucesso"),
+            @ApiResponse( responseCode = "400", description = "erro do cliente"),
+            @ApiResponse( responseCode = "500", description = "erro do servidor")
+    })
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
