f8729ecddf8cc5b8878e3d99859a898e8f113282
Bruno Antônio Joanucci <bruno.joanucci@aluno.imepac.edu.br>
2024-07-02 14:23:16 -0300

    Logger agendamento pronto

--- clinica-agendamento/src/main/java/br/edu/imepac/controllers/AgendaController.java
+++ clinica-agendamento/src/main/java/br/edu/imepac/controllers/AgendaController.java
@@ -4,6 +4,8 @@ import br.edu.imepac.dtos.AgendaConsultaCreateRequest;
 import br.edu.imepac.dtos.AgendaConsultaDto;
 import br.edu.imepac.services.AgendaService;
 import org.apache.coyote.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
@@ -14,18 +16,24 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 
+
 @RestController
 @RequestMapping("/agendamento")
 public class AgendaController {
+
+    private static final Logger logger = LoggerFactory.getLogger(AgendaController.class);
     @Autowired
     private AgendaService agendaService;
 
     @GetMapping
     public ResponseEntity<List<AgendaConsultaDto>> findAll(){
         List<AgendaConsultaDto> list = agendaService.findAll();
+        logger.info("Agendamentos encontrados: {}", list);
         if(list.isEmpty()){
+            logger.info("NOT FOUND");
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }else {
+            logger.info("OK");
             return new ResponseEntity<>(list, HttpStatus.OK);
         }
     }
@@ -33,9 +41,12 @@ public class AgendaController {
     @GetMapping("/{id}")
     public ResponseEntity<AgendaConsultaDto> findById(@PathVariable Long id){
         AgendaConsultaDto agendaConsultaDto = agendaService.findById(id);
+        logger.info("Agendamento encontrado por id: {}", agendaConsultaDto);
         if(agendaConsultaDto != null){
+            logger.info("OK");
             return new ResponseEntity<>(agendaConsultaDto, HttpStatus.OK);
         }else {
+            logger.info("NOT FOUND");
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
@@ -45,12 +56,14 @@ public class AgendaController {
         try {
             AgendaConsultaDto agendaConsultaDto = agendaService.createConsulta(agendaConsultaCreateRequest);
             if (agendaConsultaDto != null) {
+                logger.info("Agendamento salvo: {}", agendaConsultaDto);
                 return new ResponseEntity<>(agendaConsultaDto, HttpStatus.CREATED);
             } else {
+                logger.warn("agendaConsultaDto retornou nulo");
                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
             }
         }catch (Exception e){
-            System.out.println(e.getMessage());
+            logger.warn("Erro de conflito: {}", e.getMessage());
             return new ResponseEntity<>(HttpStatus.CONFLICT);
         }
     }
@@ -60,12 +73,14 @@ public class AgendaController {
         try {
             AgendaConsultaDto agendaConsultaDto = agendaService.update(id, agendaData);
             if (agendaConsultaDto != null) {
+                logger.info("Agendamento atualizado: {}", agendaConsultaDto);
                 return new ResponseEntity<>(agendaConsultaDto, HttpStatus.OK);
             } else {
+                logger.warn("agendaConsultaDto retornou nulo");
                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
             }
         }catch (Exception e){
-            System.out.println(e.getMessage());
+            logger.warn("Erro de conflito: {}", e.getMessage());
             return new ResponseEntity<>(HttpStatus.CONFLICT);
         }
     }
@@ -74,8 +89,10 @@ public class AgendaController {
     public ResponseEntity<AgendaConsultaDto> cancelConsulta(@PathVariable Long id, @RequestBody String motivo){
         AgendaConsultaDto agendaConsultaDto = agendaService.cancelConsulta(id, motivo);
         if(agendaConsultaDto != null){
+            logger.info("Agendamento cancelado");
             return new ResponseEntity<>(agendaConsultaDto, HttpStatus.OK);
         }else{
+            logger.warn("Cancelamento nao foi possivel");
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
@@ -85,12 +102,14 @@ public class AgendaController {
         try {
             AgendaConsultaDto agendaConsultaDto = agendaService.setReturn(agendaConsultaCreateRequest);
             if (agendaConsultaDto != null) {
+                logger.info("Retorno marcado: {}", agendaConsultaDto);
                 return new ResponseEntity<>(agendaConsultaDto, HttpStatus.CREATED);
             } else {
+                logger.warn("agendaConsultaDto retornou nulo");
                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
             }
         }catch (Exception e){
-            System.out.println(e.getMessage());
+            logger.warn("Erro de conflito: {}", e.getMessage());
             return new ResponseEntity<>(HttpStatus.CONFLICT);
         }
     }
@@ -98,7 +117,9 @@ public class AgendaController {
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteConsulta(@PathVariable Long id){
+        logger.info("Deletando Agendamento de id: {}", id);
         agendaService.deleteConsulta(id);
+        logger.info("Agendamento de id {} deletado", id);
     }
 
 
