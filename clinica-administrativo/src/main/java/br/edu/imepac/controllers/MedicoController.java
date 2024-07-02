4dd3a3c423c0add29da8cfa09bf162a1c7761f27
osmarindo <osmarindo.neto@aluno.imepac.edu.br>
2024-07-01 21:14:47 -0300

    hhh

--- clinica-administrativo/src/main/java/br/edu/imepac/controllers/MedicoController.java
+++ clinica-administrativo/src/main/java/br/edu/imepac/controllers/MedicoController.java
@@ -3,6 +3,8 @@ package br.edu.imepac.controllers;
 import br.edu.imepac.dtos.MedicoCreateRequest;
 import br.edu.imepac.dtos.MedicoDto;
 import br.edu.imepac.services.MedicoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,14 +15,20 @@ import java.util.List;
 @RestController
 @RequestMapping("medico")
 public class MedicoController {
+    private static final Logger logger = LoggerFactory.getLogger(MedicoController.class);
+
     @Autowired
     private MedicoService medicoService;
     @GetMapping
     public ResponseEntity<List<MedicoDto>> listDoctors(){
         List<MedicoDto> list = medicoService.findAll();
+        logger.info("Medicos encontrados: {}", list);
+
         if(list.isEmpty()){
+            logger.info("NOT FOUND");
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }else{
+            logger.info("OK");
             return new ResponseEntity<>(list, HttpStatus.OK);
         }
     }
@@ -29,6 +37,7 @@ public class MedicoController {
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<MedicoDto> findDoctor(@PathVariable Long id){
         MedicoDto medicoDto = medicoService.findById(id);
+        logger.info("Medico encontrado por id: {}", medicoDto);
         if(medicoDto != null){
             return new ResponseEntity<>(medicoDto, HttpStatus.OK);
         }else{
@@ -40,6 +49,7 @@ public class MedicoController {
     @PostMapping
     public ResponseEntity<MedicoDto> saveDoctor(@RequestBody MedicoCreateRequest medicoCreateRequest){
         MedicoDto medicoDto = medicoService.save(medicoCreateRequest);
+        logger.info("Medico salvo: {}", medicoDto);
         return new ResponseEntity<>(medicoDto, HttpStatus.CREATED);
     }
 
@@ -47,9 +57,12 @@ public class MedicoController {
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<MedicoDto> updateDoctor(@RequestBody MedicoDto medicoData, @PathVariable Long id){
         MedicoDto medicoDto = medicoService.update(id, medicoData);
+        logger.info("Buscando medico de id {}",id);
         if(medicoDto != null){
+            logger.info("Medico atualizado com sucesso: {}", id);
             return new ResponseEntity<>(medicoDto, HttpStatus.OK);
         }else{
+            logger.warn("Medico com id {}, não encontrado", id);
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
@@ -58,6 +71,8 @@ public class MedicoController {
     @ResponseStatus(HttpStatus.OK)
     public void deleteDoctor(@PathVariable Long id){
         medicoService.delete(id);
+        logger.info("Medico com id: {} deletado com sucesso", id);
+
     }
 
 }
